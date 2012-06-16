7f5bf94bb8ffab24b6dba08f2afb7282ee68b0f9
Science Ecneics <sciencebrainrs@gmail.com>
2012-06-15 19:38:55 -0500

    Initial Commit

---
+++ SlimeWorshipper.java
@@ -0,0 +1,12 @@
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.Manifest;
+
+@Manifest(name = "SlimeWorshipper", description = "Prays at Ectofuntus", authors = { "sciencebrain" }, version = 1.0)
+public class SlimeWorshipper extends ActiveScript {
+
+	@Override
+	protected void setup() {
+		
+	}
+	
+}
